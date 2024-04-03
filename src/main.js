d9ae16e0d509d2deabd8156ce8a9c3daf86a12b3
FM_Zuloaga <famelzuloaga@gmail.com>
2024-04-03 02:28:09 -0500

    added root in main with elementbyid-added root element in main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { example } from './dataFunctions.js';
+// import { filterData, sortData, computeStats } from './dataFunctions.js';// botones
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-console.log(example, renderItems(data), data);
+const root = document.getElementById("root")
+console.log(root.innerHTML);
+root.innerHTML = renderItems(data);
+
+
+
+// console.log(example, renderItems(data), data);
