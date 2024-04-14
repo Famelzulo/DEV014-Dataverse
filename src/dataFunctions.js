9602b77fb7aa4b067b07b23d61c1d8844c166204
FM_Zuloaga <famelzuloaga@gmail.com>
2024-04-13 23:31:49 -0500

    Added sortData function

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,63 @@
-export const example = () => {
-  return 'example';
-};
+function filterBy(data, key, value) {
+  return data.filter(item => item['facts'][key] === value);
+}
 
-export const anotherExample = () => {
-  return "OMG";
-};
-//console.log(functions.example()); // Imprime 'example'
-// console.log(functions.anotherExample()); // Imprime []
+export function filterData(data, value) {
+  console.log(data, value);
+  if (value === 'Google' || value === 'Amazon' || value === 'Personalizado') {
+    return filterBy(data, 'Empresa', value);
+  } else if (value === "") {
+    return data;
+  }
+}
+
+export function filterCategory(data, value) {
+  console.log(data, value);
+  if (value === 'Tipado' || value === 'No tipado' || value === 'Turing completo') {
+    return filterBy(data, 'category', value);
+  } else if (value === "") {
+    return data;
+  }
+}
+function compareAsc(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+
+}
+function compareDesc(a, b) {
+  if (a.name > b.name) {
+    return -1;
+  }
+  if (a.name < b.name) {
+    return 1;
+  }
+  return 0;
+
+}
+
+
+
+
+
+export function sortData(data, sortBy, sortOrder) {
+  // data.sort(function(a, b) {
+  //return (a.name) -(b.name);
+  //};
+  if (sortOrder === 'asc') {
+    data.sort(compareAsc);
+    return data;
+  } else if (sortOrder === 'desc') {
+    data.sort(compareDesc);
+    return data;
+  } else if (sortOrder === "") {
+    return data;
+  }
+
+
+}
+
